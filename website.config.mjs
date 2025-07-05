76bc9de0311768bed2994de0538c4d50fa7b73e9
BravishkaSkytano <65601594+BravishkaSkytano@users.noreply.github.com>
2025-07-05 17:45:36 -0400

    Initial commit

--- website.config.mjs
+++ website.config.mjs
@@ -1,8 +1,10 @@
+import { TAGS_COLLECTION_NAME } from "./src/constants";
 
 export default {
-  author: 'Spaceship CO',
-  base: 'astro-theme-spaceship',
-  title: 'Spaceship',
-  description: 'Astro Template for publishing your Obsidian Vault',
+  author: 'Yoela O',
+  base: '/',
+  title: "The Worldweaver's Journal",
+  tagline: 'A Chronicle of Realms and Imagination.',
+  description: 'Open the door to veil-thin worlds and dim-lit realms, where forgotten archives whisper stories lost to time. Here, shadows speak, memories stir, and every step leads deeper into the unseen. Let the echoes guide you—what was lost still lingers.',
   defaultLanguage: 'en',
 }
