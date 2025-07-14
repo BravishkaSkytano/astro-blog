02935451ed6e416377861f34c29757dde2e32a87
BravishkaSkytano <65601594+BravishkaSkytano@users.noreply.github.com>
2025-07-14 12:58:01 -0400

    fix favicon

--- website.config.mjs
+++ website.config.mjs
@@ -2,7 +2,7 @@ import { TAGS_COLLECTION_NAME } from "./src/constants";
 
 export default {
   author: 'Yoela O',
-  base: '/',
+  base: '',
   title: "The Worldweaver's Journal",
   tagline: 'A Chronicle of Realms and Imagination.',
   description: 'Open the door to veil-thin worlds and dim-lit realms, where forgotten archives whisper stories lost to time. Here, shadows speak, memories stir, and every step leads deeper into the unseen. Let the echoes guide you—what was lost still lingers.',
